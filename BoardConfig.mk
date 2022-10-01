fb12f30c1ecd31cbe3b99fbf2f8f865a6cdabdf3
Arafattex <arafattex45558@gmail.com>
2022-10-01 09:34:29 +0600

    ni need apex

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -112,3 +112,4 @@ TW_USE_TOOLBOX := true
 TW_BRIGHTNESS_PATH := "/sys/devices/platform/leds-mt65xx/leds/lcd-backlight/brightness"
 TW_MAX_BRIGHTNESS := 2047
 TW_DEFAULT_BRIGHTNESS := 205
+TW_EXCLUDE_APEX := true
