98ce4a62611b15c1ff5e536f4f828bdaa5f4664f
Arafattex <69374795+Arafattex@users.noreply.github.com>
2022-10-09 11:35:08 +0600

    usbconfigfs=true

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -80,7 +80,8 @@ BOARD_USES_METADATA_PARTITION := true
 
 # Kernel
 TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/Image.gz
-BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 androidboot.selinux=permissive.usbconfigfs=true
+BOARD_KERNEL_CMDLINE += androidboot
 BOARD_KERNEL_IMAGE_NAME := Image.gz
 BOARD_KERNEL_BASE := 0x40078000
 BOARD_KERNEL_PAGESIZE := 2048
